20fc21aac9c0bbbb1f5848062574dc63e22c414f
Daniel Schulz <daniel.schulz@spiegel.de>
2020-05-02 23:17:28 +0200

    add webmentions js

--- layouts/partials/webmentions/webmentions.js
+++ layouts/partials/webmentions/webmentions.js
@@ -7,6 +7,7 @@ export default class Webmentions extends Component {
 
     init() {
         if (!this.apiProxyUrl) { return; }
+        this.replyListHTML = "";
         this.getWebmentions();
     }
 
@@ -15,9 +16,36 @@ export default class Webmentions extends Component {
         const apiFetchUrl = this.apiProxyUrl + encodeURIComponent(webmentionsFetchUrl);
 
         fetch(apiFetchUrl)
-        .then(response => response.json())
-        .then((data) => {
-            console.log(data)
-        });
+            .then(response => response.json())
+            .then((data) => {
+                Array.from(data.children).forEach(reply => this.addReply(reply));
+                this.showReplies();
+            });
+    }
+
+    addReply(replyData) {
+        if (!replyData.content.html || !replyData.author.name) { return; }
+
+        let reply = document.createElement('template');
+        reply.innerHTML = this.prototype.outerHTML;
+        let replyLink = reply.content.firstChild.querySelector('[data-webmentions-el="link"]');
+        let replyAvatar = reply.content.firstChild.querySelector('[data-webmentions-el="avatar"]');
+        let replyName = reply.content.firstChild.querySelector('[data-webmentions-el="name"]');
+        let replyDate = reply.content.firstChild.querySelector('[data-webmentions-el="date"]');
+        let replyContent = reply.content.firstChild.querySelector('[data-webmentions-el="content"]');
+
+        replyName.innerHTML = replyData.author.name;
+        replyContent.innerHTML = replyData.content.html;
+        replyLink.href = replyData.author.url || "";
+        replyAvatar.src = replyData.author.photo || "";
+        replyDate.innerHTML = replyData.published || "some time";
+
+        reply.content.firstChild.removeAttribute('hidden');
+        this.replyListHTML += reply.content.firstChild.outerHTML;
+    }
+
+    showReplies() {
+        this.loader.setAttribute('hidden', 'hidden');
+        this.replies.insertAdjacentHTML('beforeend', this.replyListHTML);
     }
 }
