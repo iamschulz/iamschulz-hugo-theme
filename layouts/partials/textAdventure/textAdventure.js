7be3d4141d4b009c26b1f9068a35a5891b7993dc
Daniel Schulz <daniel.schulz@spiegel.de>
2020-07-08 23:41:44 +0200

    add items json

--- layouts/partials/textAdventure/textAdventure.js
+++ layouts/partials/textAdventure/textAdventure.js
@@ -1,5 +1,6 @@
 import Component from '../../../helpers/component';
 import * as ROOMS from './rooms.json';
+import * as ITEMS from './items.json';
 
 class Room {
     constructor(config) {
@@ -10,9 +11,19 @@ class Room {
     }
 }
 
+class Item {
+    constructor(config) {
+        this.name = config.name || "Unnamed item";
+        this.description = config.description || "";
+        this.interaction = config.interaction || "";
+        this.method = config.method || "";
+    }
+}
+
 export default class TextAdventure extends Component {
     init() {
         this.setupRooms();
+        this.setupItems();
         this.setupGlobals();
     }
 
@@ -45,6 +56,15 @@ export default class TextAdventure extends Component {
         this.roomsState = new StateMachine(this, roomsStateConfig);
     }
 
+    setupItems() {
+        this.items = {};
+        Object.keys(ITEMS.default).forEach(item => {
+            this.items[item] = new Item(ITEMS.default[item]);
+            
+            // register interaction method here
+        });
+    }
+
     /* ACTIONS */
     help() {
         console.warn('help is not implemented yet');
@@ -55,16 +75,20 @@ export default class TextAdventure extends Component {
         const room = this.rooms[this.roomsState.states.room.currentState];
 
         if (validDirections.indexOf(direction) < 0) {
-            console.log("you may only go", validDirections);
+            console.log("You may only go", validDirections);
             return;
         }
 
         if (Object.keys(room.directions).indexOf(direction) < 0) {
-            console.log(`you can't go ${direction} from here.`);
+            console.log(`You can't go ${direction} from here.`);
             return;
         }
 
-        const newRoom = room.directions[direction];
+        const newRoom = room.directions[direction].destination;
+        console.log(
+            room.directions[direction].transition,
+            this.rooms[newRoom].intro
+        );
         EventBus.publish(`change${newRoom[0].toUpperCase() + newRoom.substring(1)}`, this.el);
     }
 
@@ -74,8 +98,7 @@ export default class TextAdventure extends Component {
 
     /* GAME LOGIC */
     onChangeRoom() {
-        const room = this.roomsState.states.room.currentState;
-        console.log(this.rooms[room].intro);
+        // perpetuate state here
     }
 
     /* EXIT GAME */
