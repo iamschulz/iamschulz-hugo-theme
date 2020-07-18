5f2405a074ae62e68ed1118476606ebffc935556
Daniel Schulz <daniel.schulz@spiegel.de>
2020-07-18 22:57:46 +0200

    persist state

--- layouts/partials/textAdventure/textAdventure.js
+++ layouts/partials/textAdventure/textAdventure.js
@@ -24,7 +24,9 @@ export default class TextAdventure extends Component {
         this.setupRooms();
         this.setupItems();
         this.setupGlobals();
-        this.startGame();
+        window.setTimeout(() => {
+            this.startGame();
+        }, 0);
     }
 
     /* SETUP GAME */
@@ -33,14 +35,16 @@ export default class TextAdventure extends Component {
             help: this.help.bind(this),
             go: this.movePC.bind(this),
             inspect: this.inspect.bind(this),
-            use: this.use.bind(this)
+            use: this.use.bind(this),
+            reset: this.resetGame.bind(this)
         }
     }
 
     setupRooms() {
+        const startingRoom = sessionStorage.ta ? JSON.parse(sessionStorage.ta).rooms : 'Lobby';
         const roomsStateConfig = {
             room: {
-                value: 'Lobby' // starting room
+                value: startingRoom
             }
         }
 
@@ -71,7 +75,7 @@ export default class TextAdventure extends Component {
 
     movePC(direction) {
         const validDirections = ['north', 'east', 'south', 'west'];
-        const room = this.rooms[this.roomsState.states.room.currentState];
+        const room = this.getCurrentRoom();
 
         if (validDirections.indexOf(direction) < 0) {
             console.log("You may only go", validDirections);
@@ -89,12 +93,14 @@ export default class TextAdventure extends Component {
             this.rooms[newRoom].description
         );
         EventBus.publish(`taEnter${newRoom[0].toUpperCase() + newRoom.substring(1)}`, this.el);
+
+        this.persistState();
     }
 
     inspect(thing) {
         if (!thing) {
-            console.log(`You take a look around the ${this.roomsState.states.room.currentState}.`);
-            this.inspect(this.roomsState.states.room.currentState);
+            console.log(`You take a look around the ${this.getCurrentRoom().name}.`);
+            this.inspect(this.getCurrentRoom().name);
             return;
         }
         if (this.rooms[thing]) {
@@ -115,7 +121,7 @@ export default class TextAdventure extends Component {
         const item = this.items[
             Object.keys(this.items).filter(item => item.toLowerCase() === thing.toLowerCase())[0]
         ];
-        const thingInRoom = Object.keys(this.rooms[this.roomsState.states.room.currentState].objects)
+        const thingInRoom = Object.keys(this.getCurrentRoom().objects)
             .filter(item => item.toLowerCase() === thing.toLowerCase())
             .length > 0;
 
@@ -137,11 +143,24 @@ export default class TextAdventure extends Component {
 
     /* GAME LOGIC */
     startGame() {
-        console.log('You find yourself in a mansion.');
-        ta.inspect('Lobby');
+        console.log(`You find yourself in a mansions ${this.getCurrentRoom().name}`);
+        ta.inspect(this.getCurrentRoom().name);
+    }
+
+    persistState() {
+        sessionStorage.ta = JSON.stringify({
+            rooms: this.getCurrentRoom().name
+        });
+    }
+
+    getCurrentRoom() {
+        const currentRoom = this.rooms[this.roomsState.states.room.currentState]
+        currentRoom.name = this.roomsState.states.room.currentState;
+        return currentRoom;
     }
 
     onEnterRoom() {
+        this.persistState();
     }
 
     /**
@@ -152,9 +171,17 @@ export default class TextAdventure extends Component {
         console.warn('Articles are not yet implemented');
     }
 
+    resetGame() {
+        this.destroy();
+        this.init();
+        window.setTimeout(() => {
+            this.persistState();
+        }, 0);
+    }
+
     /* EXIT GAME */
     destroy() {
-        window.t = undefined;
-        window.ta = undefined;
+        delete window.ta;
+        delete sessionStorage.ta;
     }
 }
