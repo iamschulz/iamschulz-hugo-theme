f4b1b6d92335d52d2d1511911bca0b44c9a8ee8e
daniel schulz <daniel.schulz@baqend.com>
2021-05-05 00:03:06 +0200

    slighly better html

--- helpers/legacy.es5.js
+++ helpers/legacy.es5.js
@@ -1,6 +1,8 @@
 // fix lazy loaded images
-var lazyImages = document.querySelectorAll('img[src=""]');
+var lazyImages = document.querySelectorAll(
+	'img[src=""],img[src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"]'
+);
 for (i = 0; i < lazyImages.length; ++i) {
-	lazyImages[i].setAttribute("src", lazyImages[i].dataset.src);
+	lazyImages[i].setAttribute('src', lazyImages[i].dataset.src);
 	console.log(lazyImages[i]);
 }
